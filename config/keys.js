2edf4935bf523c666becc305f6df221a9b22a7d9
benjamin chukwudi <73422820+benjaminugwuanyi@users.noreply.github.com>
2023-02-13 22:08:02 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword22 = 'mongodb://localhost:27017/mern2';
+dbPassword= 'mongodb+srv://rocketwalletsignal:HelloToday@cluster0.9l7ec.mongodb.net/test'
+
+module.exports = {
+    mongoURI: dbPassword
+};
